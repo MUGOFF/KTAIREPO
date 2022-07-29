d3455c6809a701d34c8bc89032c8bcb2b5f05eed
Moon <mek0223@navr.com>
2022-07-29 14:49:25 +0900

    day3.6

---
+++ 3days_py/Plus_prac.py
@@ -0,0 +1,10 @@
+# Q0 Answer template
+
+# N, X = map(int, input('N, X').split())  # N, X 를 입력받음
+# data = list(map(int, input("list").split())) # 리스트를 입력받음
+# answer = []
+# for i in range(N):
+#     if data[i]<X:
+#         answer.append(data[i])
+# print(answer)
+#######################################################

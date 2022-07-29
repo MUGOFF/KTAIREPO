9d7ee124a2a0d66965db0ba9175147c55f2135db
Moon <mek0223@navr.com>
2022-07-29 10:40:31 +0900

    day3.1

--- 3days_py/Mini_prac.py
+++ 3days_py/Mini_prac.py
@@ -1,8 +1,25 @@
-def min_of(li):
-    ans = li[0]
-    for i in range(li):
-        if i<ans:
-            ans =i
-            
-    return ans
+#MINIQ1
+# def min_of(li):
+#     ans = li[0]
+#     for i in li:
+#         if i <ans:
+#             ans = i
+#     return ans
+# t = (4, 7, 5.6, 2, 3.14, 1)
+# s = 'string'
+# a = ['DTS', 'AAC', 'FLAC']
+# print(min_of(t))
+# print(min_of(s))
+# print(min_of(a))
+#MINIQ2
+# def rev_pri(li):
+#     print('리스트 역순 출력')
+#     for i in range(len(li)-1,-1,-1):
+#         print(li[i])
+# num = int(input('원소 수를 입력하세요.: '))
+# x = [None] * num # 원소 수가 num인 리스트를 생성
+# for i in range(num):
+#     x[i] = int(input(f'x[{i}]값을 입력하세요.: '))
+# rev_pri(x)
+#MINIQ1
     
